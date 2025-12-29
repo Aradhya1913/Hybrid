b2dbe2b4e0ca170004a60d3576f8b22ebb01c291
Aradhya R <nandeesharadhya19@gmail.com>
2025-12-30 01:28:58 +0530

    Butttons Fix

--- src/data/scenes.ts
+++ src/data/scenes.ts
@@ -20,7 +20,7 @@ const scenes: SceneDef[] = [
     title: 'Inside Gate', 
     url: '/panos/spot2.jpg',
     hotSpots: [
-      { yaw: 90, pitch: -25, targetSceneId: 'spot1', text: 'Back' },
+      { yaw: 80, pitch: -25, targetSceneId: 'spot1', text: 'Back' },
       { yaw: 290, pitch: -10, targetSceneId: 'spot3', text: 'Forward' },
       { yaw: 210, pitch: -7, targetSceneId: 'spot2b', text: 'Right' }
     ]
@@ -30,9 +30,9 @@ const scenes: SceneDef[] = [
     title: 'Road Split', 
     url: '/panos/spot2.2.jpg',
     hotSpots: [
-      { yaw: 0, pitch: 0, targetSceneId: 'spot2', text: 'Back' },
-      { yaw: 45, pitch: 0, targetSceneId: 'spot5', text: 'Library' },
-      { yaw: -45, pitch: 0, targetSceneId: 'spot4', text: 'Playground' }
+      { yaw: 15, pitch: -7, targetSceneId: 'spot2', text: 'Back' },
+      { yaw: 175, pitch: -7, targetSceneId: 'spot2.2', text: 'Back' }, // ADD BOYS HOSTEL
+      { yaw: 265, pitch: -11, targetSceneId: 'spot9', text: 'CSE' }, 
     ]
   },
   { 
@@ -40,45 +40,104 @@ const scenes: SceneDef[] = [
     title: 'Globe', 
     url: '/panos/spot3.jpg',
     hotSpots: [
-      { yaw: 180, pitch: 0, targetSceneId: 'spot1', text: 'Road' },
-      { yaw: 90, pitch: 0, targetSceneId: 'spot2', text: 'Gate' },
-      { yaw: 0, pitch: 0, targetSceneId: 'spot6', text: 'Building' }
+      { yaw: 90, pitch: 0, targetSceneId: 'spot2', text: 'Road' },
+      { yaw: 270, pitch: -4, targetSceneId: 'spot4', text: 'Road' },
+
+   
     ]
   },
   { 
     id: 'spot4', 
-    title: 'Playground', 
+    title: 'TM manjunath corner', 
     url: '/panos/spot4.jpg',
     hotSpots: [
-      { yaw: 0, pitch: 0, targetSceneId: 'spot1', text: 'Road' },
-      { yaw: 90, pitch: 0, targetSceneId: 'spot5', text: 'Library' },
-      { yaw: 180, pitch: 0, targetSceneId: 'spot2b', text: 'Road Split' }
+      { yaw: 40, pitch: -13, targetSceneId: 'spot4b', text: 'Road' },
+      { yaw: 140, pitch: -13, targetSceneId: 'spot5b', text: 'Road' },
+      { yaw: 140, pitch: -13, targetSceneId: 'spot5b', text: 'Road' },
+
     ]
   },
+  
+    { 
+    id: 'spot4b', 
+    title: 'BMW corner', 
+    url: '/panos/spot4.2.jpg',
+    hotSpots: [
+      { yaw: 40, pitch: -13, targetSceneId: 'spot4c', text: 'Road' },
+ 
+    ]
+  },
+
+      { 
+    id: 'spot4c', 
+    title: 'Office', 
+    url: '/panos/spot4.3.jpg',
+    hotSpots: [
+      { yaw: -90, pitch: -13, targetSceneId: 'spot5', text: 'Road' },
+      { yaw: 90, pitch: -13, targetSceneId: 'spot4b', text: 'Road' },
+      { yaw: 0, pitch: -13, targetSceneId: 'spot6', text: 'Road' },
+
+
+      
+
+    ]
+  },
+
   { 
     id: 'spot5', 
-    title: 'Library', 
+    title: 'Auditorim corner', 
     url: '/panos/spot5.jpg',
     hotSpots: [
-      { yaw: 45, pitch: 0, targetSceneId: 'spot4', text: 'Playground' },
-      { yaw: -45, pitch: 0, targetSceneId: 'spot5_2', text: 'Library Alt' },
-      { yaw: 180, pitch: 0, targetSceneId: 'spot2b', text: 'Back' }
+      { yaw: 4, pitch: -10, targetSceneId: 'spot7', text: 'Playground' },
+      { yaw: -90, pitch: -10, targetSceneId: 'spot4c', text: 'Playground' },
+
     ]
   },
   { 
-    id: 'spot5_2', 
-    title: 'Library alt', 
+    id: 'spot5b', 
+    title: 'Restroom corner', 
     url: '/panos/spot5.2.jpg',
     hotSpots: [
-      { yaw: 0, pitch: 0, targetSceneId: 'spot5', text: 'Library' },
-      { yaw: 90, pitch: 0, targetSceneId: 'spot6', text: 'Building' }
+      { yaw: 94, pitch: -5, targetSceneId: 'spot4', text: 'Building' },
+      { yaw: 190, pitch: -10, targetSceneId: 'spot5', text: 'Building' }
+
+      
     ]
   },
-  { id: 'spot6', title: 'Spot 6', url: '/panos/spot6.jpg' },
-  { id: 'spot7', title: 'Spot 7', url: '/panos/spot7.jpg' },
+  { id: 'spot6', title: 'Admin Lawn', url: '/panos/spot6.jpg' ,
+        hotSpots: [
+      { yaw: 94, pitch: -5, targetSceneId: 'spot5b', text: 'Building' },
+
+      
+    ]
+  },
+  { id: 'spot7', 
+    title: 'Auditorium',
+     url: '/panos/spot7.jpg' 
+     ,hotSpots: [
+      { yaw: 260, pitch: -15, targetSceneId: 'spot9', text: 'Library' },
+     ]
+  },
   { id: 'spot8', title: 'Spot 8', url: '/panos/spot8.jpg' },
-  { id: 'spot9', title: 'Spot 9', url: '/panos/spot9.jpg' },
-  { id: 'spot10', title: 'Spot 10', url: '/panos/spot10.jpg' },
+ 
+  { id: 'spot9', 
+    title: 'CSE', 
+    url: '/panos/spot9.jpg', 
+    hotSpots: [
+      { yaw: 90, pitch: -10, targetSceneId: 'spot2b', text: 'Library' },
+      { yaw: 60, pitch: 0, targetSceneId: 'spot7', text: 'Library' },
+      { yaw: 180, pitch: -10, targetSceneId: 'spot10', text: 'Library' },
+      { yaw: 270, pitch: -7, targetSceneId: 'spot11', text: 'Building' }
+
+    ]},
+
+  { id: 'spot10', title: 'Spot 10', url: '/panos/spot10.jpg' 
+      ,hotSpots: [
+      { yaw: 90, pitch: -10, targetSceneId: 'spot9', text: 'Library' },
+      
+
+    ]
+  },
   { id: 'spot11', title: 'Spot 11', url: '/panos/spot11.jpg' }
 ];
 
